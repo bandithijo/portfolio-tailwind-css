bfe767814c777110edee926aba9e297b427b46cd
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-02-20 20:18:05 +0800

    feat: Add close menu outside menu area

--- dist/js/script.js
+++ dist/js/script.js
@@ -22,4 +22,12 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
+});
+
+// close menu outside menu
+window.addEventListener('click', function(e) {
+  if(e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
 })
